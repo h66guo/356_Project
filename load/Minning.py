7992c9a4158f4225e39e706b4bf5c4651ec7ceb3
Martin Guo <h66guo@uwaterloo.ca>
2021-04-15 05:43:32 +0800

    got classification tree

--- load/Minning.py
+++ load/Minning.py
@@ -30,6 +30,8 @@ def main():
     del pima[' Destination IP']
     del pima[' Timestamp']
     del pima['External IP']
+    del pima['Flow Bytes/s']
+    del pima[' Flow Packets/s']
 # delete attributes that could not be converted to float since the api is trying to convert all the strings to float for making the decision tree
     X = pima # Features
     
@@ -51,10 +53,10 @@ def main():
 
     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-    ot_data = StringIO()
+    dot_data = StringIO()
     export_graphviz(clf, out_file=dot_data,  
                     filled=True, rounded=True,
-                    special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+                    special_characters=True,feature_names = pima.columns,class_names=['0','1'])
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
     graph.write_png('diabetes.png')
     Image(graph.create_png())
